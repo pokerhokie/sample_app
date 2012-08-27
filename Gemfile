ef3a461877df3e0858366552d98f168b8295e776
Skip Davis <skipdavis+git@gmail.com>
2012-08-26 20:55:40 -0700

    finished chapter 3 Exercises

--- Gemfile
+++ Gemfile
@@ -1,26 +1,24 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'passenger', '3.0.15'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development, :test do
-  gem 'passenger', '3.0.15'
   gem 'mysql2'
   gem 'rspec-rails', '2.10.0'
+  gem 'guard-rspec', '0.5.5' 
 end
 
 
 # Gems used only for assets and not required
 # in production environments by default.
+
 group :assets do
   gem 'sass-rails',   '3.2.4'
   gem 'coffee-rails', '3.2.2'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '1.2.3'
 end
 
@@ -28,6 +26,10 @@ gem 'jquery-rails'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App |" }
   
   describe "Home page" do
     
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', :text => 'My Sample App')
     end
     
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Home")
+                    :text => "#{base_title} Home")
     end
     
   end
@@ -27,7 +29,7 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | Help")
+                    :text => "#{base_title} Help")
     end
  
   end
@@ -41,7 +43,20 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "#{base_title} About Us")
+    end
+  end
+  
+    describe "Contact Us" do
+    it "should have the content 'Contact Information'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Information')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "#{base_title} Contact Us")
     end
   end
   
