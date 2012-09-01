b0b064d212e5befdaf64537497147e03b9988b3c
Skip Davis <skipdavis+git@gmail.com>
2012-09-01 03:11:05 -0400

    added core user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
+  
   def new
     @user = User.new
   end
@@ -15,6 +19,54 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated successfully"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
+private
+  
+  def signed_in_user
+    if !signed_in?
+      store_location
+      flash[:notice] = "Please sign in"
+      redirect_to signin_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:notice] = "Can't update another user"
+      redirect_to(@user || root_path)
+    end 
+  end
+  
+  def admin_user
+    if !current_user.admin?
+      flash[:notice] = "Only an admin can delete another user"
+      redirect_to(users_url)
     end
   end
   
