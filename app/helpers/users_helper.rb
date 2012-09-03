955321f1e85a188b947c3223fbfcbd2ac5720a10
Skip Davis <skipdavis+git@gmail.com>
2012-09-03 08:44:02 -0400

    fixed the avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   def gravatar_for(user, options = { size: 40 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = options[:size]
+    size = options[:size].to_s + 'x' + options[:size].to_s
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", size: size)
   end
 end
